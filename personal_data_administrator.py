cd94ecc31fb4dc22ffe54b8d4730eb481c459a6a
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-01-19 13:45:38 +0100

    formal errors are corrected in personal_data_administrator.py

--- personal_data_administrator.py
+++ personal_data_administrator.py
@@ -1,27 +1,30 @@
 import validators
-#print(validators.input_postal_code("Please enter your postal code (Enter 'q' to quit): ", 1010, 1020, 1030))
-#print(validators.input_bounded_integer("Please enter ", "age", 0, 130))
+
+# print(validators.input_postal_code("Please enter your postal code (Enter 'q' to quit): ", 1010, 1020, 1030))
+# print(validators.input_bounded_integer("Please enter ", "age", 0, 130))
 person = {}
 while True:
-    selection = input( "Please choose which aspect to edit: (n)ame, (d)ay of birth, (m)onth of birth, (y)ear of birth, (p)ostal code (or enter 'q' to quit):").upper()
+    selection = input(
+        "Please choose which aspect to edit: (n)ame, (d)ay of birth, (m)onth of birth, (y)ear of birth, (p)ostal code (or enter 'q' to quit):").upper()
 
     match selection:
         case "NAME" | "N":
-            #print (validators.input_string("Please Enter Name, Enter Q to Quit: "))
-            person["name"]= validators.input_string("Please Enter Name, Enter Q to Quit: ")
+            # print (validators.input_string("Please Enter Name, Enter Q to Quit: "))
+            person["name"] = validators.input_string("Please Enter Name, Enter Q to Quit: ")
         case "DAY OF BIRTH" | "DAY" | "D":
-            #print(validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31))
-            person["Day"]=validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31)
+            # print(validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31))
+            person["Day"] = validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31)
         case "MONTH OF BIRTH" | "MONTH" | "M":
-            #print(validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12))
-            person["month"]=validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12)
+            # print(validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12))
+            person["month"] = validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12)
         case "YEAR OF BIRTH" | "YEAR" | "Y":
-            #print(validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022))
-            person["year"]=validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022)
+            # print(validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022))
+            person["year"] = validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022)
         case "POSTAL CODE" | "POSTAL" | "P":
-            #print(validators.input_postal_code("Please enter your postal "
-                                              # "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
-            person["postal]=validators.input_postal_code(" Please enter your postal code Enter to quit:", 1010, 1020, 1030, 1040, 1050)
+            # print(validators.input_postal_code("Please enter your postal "
+            # "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
+            person["postal"] = validators.input_postal_code("Please enter your postal code"
+                                                            " Enter to quit:", 1010, 1020, 1030, 1040, 1050)
         case 'QUIT' | "Q":
             break
         case _:
