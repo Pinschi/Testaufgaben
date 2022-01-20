b5a1e56017d92e45ae55649e79cf88b73816a603
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-01-20 13:11:35 +0100

    cosmetic  and exception for the case if the csv file is already opened.

--- personal_data_administrator.py
+++ personal_data_administrator.py
@@ -1,40 +1,40 @@
 import validators
 import csv
-#print(validators.input_postal_code("Please enter your postal code (Enter 'q' to quit): ", 1010, 1020, 1030))
-#print(validators.input_bounded_integer("Please enter ", "age", 0, 130))
-person = {"name": None, "Day":None,"month":None,"year":None,"postal":None}
+
+person = {"Name": None, "Day": None, "Month": None, "Year": None, "Postal": None}
 while True:
-    selection = input( "Please choose which aspect to edit: (n)ame, (d)ay of birth, (m)onth of birth, (y)ear of birth, (p)ostal code (or enter 'q' to quit):").upper()
+    selection = input(
+        "Please choose which aspect to edit: (n)ame, (d)ay of birth, (m)onth of birth, (y)ear of birth, (p)ostal code (or enter 'q' to quit):").upper()
 
     match selection:
         case "NAME" | "N":
-            #print (validators.input_string("Please Enter Name, Enter Q to Quit: "))
-            person["name"]= validators.input_string("Please Enter Name, Enter Q to Quit: ")
+            person["Name"] = validators.input_string("Please Enter Name, Enter Q to Quit: ")
         case "DAY OF BIRTH" | "DAY" | "D":
-            #print(validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31))
-            person["Day"]=validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31)
+            person["Day"] = validators.input_bounded_integer("Please Enter ", "Day of birth", 1, 31)
         case "MONTH OF BIRTH" | "MONTH" | "M":
-            #print(validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12))
-            person["month"]=validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12)
+            person["Month"] = validators.input_bounded_integer("Please Enter ", "Month of birth", 1, 12)
         case "YEAR OF BIRTH" | "YEAR" | "Y":
-            #print(validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022))
-            person["year"]=validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022)
+            person["Year"] = validators.input_bounded_integer("Please Enter ", "Year of birth", 1850, 2022)
         case "POSTAL CODE" | "POSTAL" | "P":
-            #print(validators.input_postal_code("Please enter your postal "
-                                              #"code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050))
-            person["postal"]=validators.input_postal_code("Please enter your postal "
-                                               "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050)
+            person["Postal"] = validators.input_postal_code("Please enter your postal "
+                                                            "code (Enter 'q' to quit): ", 1010, 1020, 1030, 1040, 1050)
         case "QUIT" | "Q":
             break
         case _:
             print("Not valid option! Please try it again!")
 
+answer=""
 
-with open('people.csv', 'w', newline='', encoding="UTF-8") as csvfile:
-         writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-         key_list = list (person.keys())
-         writer.writerow(person.keys())
-         writer.writerow([person["name"],person["Day"], person["month"], person["year"],person["postal"]])
+while answer.upper() != "Q":
+    try:
+        with open('people.csv', 'w', newline='', encoding="UTF-8") as csvfile:
+            writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+            writer.writerow(person.keys())
+            writer.writerow([person["Name"], person["Day"], person["Month"], person["Year"], person["Postal"]])
+            answer="Q"
+    except PermissionError:
+        print("\ncsv file exists and open. Please close the file!")
+        answer=input("\nPlease press Enter if you closed the file or (Q) to quit! : ")
 
 print(person)
 print("Thank you for using the Validator!")
