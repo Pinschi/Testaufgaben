f37f9acfab89361b53c041b214fe3edebd9353a8
Wolfgang Bures <wolfgang.bures1@chello.at>
2022-02-08 21:08:41 +0100

    test cases bubble sort

--- test_bubble_sort.py
+++ test_bubble_sort.py
@@ -1,14 +1,16 @@
 
 from bubble_sort import bubble_sort
+# tests for statement coverage
 
 def test_statement_1():
+    assert bubble_sort([9, 1,5, 4, 2]) == [1,2,4,5,9]
+
+
+# tests for branch coverage
+
+def test_branch_1():
     assert bubble_sort([]) == []
 
-def test_statement_2():
+def test_branch_2():
     assert bubble_sort([8]) == [8]
 
-def test_statement_3():
-    assert bubble_sort([8, 7, 5]) == [5,7,8]
-
-def test_statement_4():
-    assert bubble_sort([9, 1,5, 4, 2]) == [1,2,4,5,9]
